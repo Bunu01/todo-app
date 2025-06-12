168f1d1d00d9894aebc8366cb749d6c5697d3ce0
Bunu01 <kumarmayank7215@gmail.com>
2025-06-12 11:23:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default {
+  preview: {
+    port: process.env.PORT || 5173,
+    host: true, // this ensures it binds to 0.0.0.0
+  }
+}
